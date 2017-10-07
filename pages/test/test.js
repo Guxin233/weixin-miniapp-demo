8f8ffbac999b30c32783a18090b81edc5c4cb210
guxin <525205430@qq.com>
2017-10-07 17:33:12 +0800

    update test for code

--- pages/test/test.js
+++ pages/test/test.js
@@ -6,7 +6,17 @@ var pageObject = {
     warnSize: 'default',
     disabled: false,
     plain: false,
-    loading: false
+    loading: false,
+
+		objectArray: [
+			{ id: 5, unique: 'unique_5' },
+			{ id: 4, unique: 'unique_4' },
+			{ id: 3, unique: 'unique_3' },
+			{ id: 2, unique: 'unique_2' },
+			{ id: 1, unique: 'unique_1' },
+			{ id: 0, unique: 'unique_0' },
+		],
+		numberArray: [1, 2, 3, 4]
   },
   setDisabled: function(e) {
     this.setData({
@@ -22,7 +32,35 @@ var pageObject = {
     this.setData({
       loading: !this.data.loading
     })
-  }
+  },
+
+
+	switch: function (e) {
+		const length = this.data.objectArray.length
+		for (let i = 0; i < length; ++i) {
+			const x = Math.floor(Math.random() * length)
+			const y = Math.floor(Math.random() * length)
+			const temp = this.data.objectArray[x]
+			this.data.objectArray[x] = this.data.objectArray[y]
+			this.data.objectArray[y] = temp
+		}
+		this.setData({
+			objectArray: this.data.objectArray
+		})
+	},
+	addToFront: function (e) {
+		const length = this.data.objectArray.length
+		this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
+		this.setData({
+			objectArray: this.data.objectArray
+		})
+	},
+	addNumberToFront: function (e) {
+		this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+		this.setData({
+			numberArray: this.data.numberArray
+		})
+	}
 }
 
 for (var i = 0; i < types.length; ++i) {
