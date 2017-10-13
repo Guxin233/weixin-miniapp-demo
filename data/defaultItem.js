7ef74f0431f58f4ec6605af13eeae36d98c6be3c
guxin <525205430@qq.com>
2017-10-13 15:52:10 +0800

    点击Item 弹出操作菜单

--- data/defaultItem.js
+++ data/defaultItem.js
@@ -1,8 +1,7 @@
 var list = [
     { id: 1, content: '点击底部【+】添加新的事项' },
-    { id: 2, content: '点击事项进入专注模式' },
-    { id: 3, content: '左滑移除/置顶/完成' },
-    { id: 4, content: '我是已完成的事项', finish: 'fn' },
+    { id: 2, content: '点击事项弹出操作菜单' },
+    { id: 3, content: '我是已完成的事项', finish: 'fn' },
 ];
 
 module.exports = list;
--- pages/todoList/todoList.js
+++ pages/todoList/todoList.js
@@ -191,6 +191,8 @@ Page({
 
   // 点击Item，进入专注模式
   focusMode: function (e){
+
+		return;
     // 已完成的事项不能进入专注模式！
     var item = listHelper.getItemById(all.list, e.currentTarget.dataset.id);
     if (item.finish != null && item.finish == "fn"){
@@ -202,7 +204,62 @@ Page({
     wx.navigateTo({
       url: '../focusMode/focusMode?id=' + e.currentTarget.dataset.id,
     });
-  }
+  },
+
+	popupMenu: function (e) {
+		var currentStatu = e.currentTarget.dataset.statu;
+		this.util(currentStatu)
+	},
+
+	util: function (currentStatu) {
+		/* 动画部分 */
+		// 第1步：创建动画实例 
+		var animation = wx.createAnimation({
+			duration: 200, //动画时长 
+			timingFunction: "linear", //线性 
+			delay: 0 //0则不延迟 
+		});
+
+		// 第2步：这个动画实例赋给当前的动画实例 
+		this.animation = animation;
+
+		// 第3步：执行第一组动画 
+		animation.opacity(0).rotateX(-100).step();
+
+		// 第4步：导出动画对象赋给数据对象储存 
+		this.setData({
+			animationData: animation.export()
+		})
+
+		// 第5步：设置定时器到指定时候后，执行第二组动画 
+		setTimeout(function () {
+			// 执行第二组动画 
+			animation.opacity(1).rotateX(0).step();
+			// 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
+			this.setData({
+				animationData: animation
+			})
+
+			//关闭 
+			if (currentStatu == "close") {
+				this.setData(
+					{
+						showModalStatus: false
+					}
+				);
+			}
+		}.bind(this), 200)
+
+		// 显示 
+		if (currentStatu == "open") {
+			this.setData(
+				{
+					showModalStatus: true
+				}
+			);
+		}
+	} 
+
 })
 
 // 刷新显示列表
