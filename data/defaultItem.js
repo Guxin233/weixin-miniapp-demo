62621f7675458c68bf79d8a46649ee4da21ef605
guxin <525205430@qq.com>
2017-10-14 17:15:11 +0800

    改回下拉添加条目

--- data/defaultItem.js
+++ data/defaultItem.js
@@ -1,7 +1,8 @@
 var list = [
-    { id: 1, content: '点击底部【+】添加新的事项' },
+    { id: 1, content: '下拉添加新的事项' },
     { id: 2, content: '点击事项弹出操作菜单' },
-    { id: 3, content: '我是已完成的事项', finish: 'fn' },
+		{ id: 3, content: '事项最多能输入二十个汉字' },
+    { id: 4, content: '我是已完成的事项', finish: 'fn' },
 ];
 
 module.exports = list;
